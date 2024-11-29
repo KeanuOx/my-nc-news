b43fdba537454c6c1f6b83a63c622f2678e22014
Keanu <keanu@googlemail.com>
2024-11-29 10:44:28 +0000

    adds task 12

--- model.js
+++ model.js
@@ -20,35 +20,51 @@ exports.selectArticleById = (id) =>{
     })
 }
 
-exports.selectArticles = (sort_by = "created_at", order = "desc") => {
+exports.selectArticles = (sort_by = "created_at", order = "desc", topic) => {
   const validSortBy = ["article_id", "title", "author", "topic", "created_at", "votes", "article_img_url"];
   const validOrder = ["asc", "desc"];
-
-  if (!validSortBy.includes(sort_by)) {
-    return Promise.reject({ status: 400, msg: "Bad Request" });
-  }
-  if (!validOrder.includes(order)) {
-    return Promise.reject({ status: 400, msg: "Bad Request" });
-  }
-
+  const queryValues = [];
   let sqlQuery = `
-    SELECT 
-      articles.article_id,
-      articles.title,
-      articles.author,
-      articles.topic,
-      articles.created_at,
-      articles.votes,
-      articles.article_img_url,
-      COUNT(comments.comment_id)::int AS comment_count
-    FROM articles
-    LEFT JOIN comments ON comments.article_id = articles.article_id
-    GROUP BY articles.article_id
+      SELECT 
+          articles.article_id,
+          articles.title,
+          articles.author,
+          articles.topic,
+          articles.created_at,
+          articles.votes,
+          articles.article_img_url,
+          COUNT(comments.comment_id)::int AS comment_count
+      FROM articles
+      LEFT JOIN comments ON comments.article_id = articles.article_id
   `;
 
-  sqlQuery += `ORDER BY ${sort_by} ${order}`;
+  if (topic) {
+      sqlQuery += `WHERE articles.topic = $1 `;
+      queryValues.push(topic);
+  }
 
-  return db.query(sqlQuery).then(({ rows }) => rows);
+  if (!validSortBy.includes(sort_by)) {
+      return Promise.reject({ status: 400, msg: "Bad Request" });
+  }
+
+  if (!validOrder.includes(order)) {
+      return Promise.reject({ status: 400, msg: "Bad Request" });
+  }
+
+  sqlQuery += `GROUP BY articles.article_id ORDER BY ${sort_by} ${order}`;
+
+  return db.query(sqlQuery, queryValues).then(({ rows }) => {
+      if (topic && rows.length === 0) {
+          return db
+              .query("SELECT * FROM topics WHERE slug = $1", [topic])
+              .then(({ rows }) => {
+                  if (rows.length === 0) {
+                      return Promise.reject({ status: 404, msg: "Not Found" });
+                  }
+              });
+      }
+      return rows;
+  });
 };
 
 exports.selectArticleComments =  (article_id) =>{
@@ -117,23 +133,23 @@ exports.updateArticleVotes = (article_id, inc_votes) => {
 };
 
 
-exports.removeComment = (comment_id) =>{
-  if (!comment_id){
-    return Promise.reject({ status: 400, msg: "Bad Request" })
-
-  }
+exports.removeComment = (comment_id) => {
   return db
-    .query("SELECT * FROM comments WHERE comment_id = $1;", [comment_id])
-    .then((commentCheck) => {
-      if (commentCheck.rows.length === 0) {
+    .query(
+      `
+      DELETE FROM comments
+      WHERE comment_id = $1
+      RETURNING *;
+      `,
+      [comment_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not Found" });
       }
-  return db.query(`DELETE FROM comments WHERE comment_id = $1`,[comment_id])
-  .then(()=>{
-    return({})
-  })
-})
-}
+      return {};
+    });
+};
 
 exports.selectUsers = () =>{
   return db.query(`SELECT * FROM users`)
